fed4d3ac475ce3738b10f70500f8f4d16077ef55
Eb0nY-April14 <laratemi14@gmail.com>
2021-10-10 16:37:14 +0000

    Create the view to display all items in a list

--- shoppinglist/views.py
+++ shoppinglist/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import ShoppingListItem
 
 
 # Create your views here.
 def get_shopping_list(request):
-    return render(request, 'shoppinglist/shoppinglist.html')
+    shoppinglistitems = ShoppingListItem.objects.all()
+    context = {
+        'shoppinglistitems': shoppinglistitems
+    }
+    return render(request, 'shoppinglist/shoppinglist.html', context)
